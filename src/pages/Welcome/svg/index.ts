7d01ace445a26f578f63e410640f4aac6e143569
k.filonik <kirill.filonik123@gmail.com>
2024-04-15 04:12:41 +0500

    добавил страницу онбординга

---
+++ src/pages/Welcome/svg/index.ts
@@ -0,0 +1,4 @@
+export { default as ClockSvg } from './clock.svg';
+export { default as HandsSvg }  from './hands.svg';
+export { default as GlassesSvg }  from './glasses.svg';
+export { default as HeartSvg }  from './heart.svg';
