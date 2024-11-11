cebfbcd36b0aa74083077e55c2ec907e78133c0a
Egor Polyakov <user@MacBook-Pro-user.local>
2024-11-11 21:27:52 +0500

    fix(Button): types

--- src/shared/Button/index.tsx
+++ src/shared/Button/index.tsx
@@ -1,6 +1,6 @@
-import { FC, MouseEventHandler } from 'react';
+import { FC, MouseEventHandler } from 'react'
 import styles from './button.module.css'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 export interface ButtonBaseProps {
   children: string
@@ -10,33 +10,29 @@ export interface ButtonBaseProps {
 
 interface ButtonLinkProps extends ButtonBaseProps {
   linkTo: string
-  onClick?: never
 }
 
 interface ButtonOnClickProps extends ButtonBaseProps {
-  linkTo?: never
   onClick: MouseEventHandler<HTMLAnchorElement>
 }
 
-type ButtonProps = ButtonLinkProps | ButtonOnClickProps;
+type ButtonProps = ButtonLinkProps | ButtonOnClickProps
 
-const Button: FC<ButtonProps> = ({
-  children,
-  className,
-  linkTo,
-  onClick,
-  isDisable = false }) => {
-
+const Button: FC<ButtonProps> = (props) => {
   return (
-    <Link to={linkTo} className={[styles.btn, className, isDisable ? styles.disabled : ''].join(' ')} onClick={onClick}>
+    <Link
+      to={'linkTo' in props ? props.linkTo : undefined}
+      className={[styles.btn, props.className, props.isDisable === false ? styles.disabled : ''].join(' ')}
+      onClick={'onClick' in props ? props.onClick : undefined}
+    >
       <svg width="230px" height="78" fill="none">
-        <path d="M1 1L220 5L210 69L8 77L1 1Z" stroke="#6B6C6F" strokeMiterlimit="10"
-          strokeDasharray="9 8" />
-        <text x='50%' y='70%' textAnchor='middle' fontSize='48px' fill="#6B6C6F">{children}</text>
+        <path d="M1 1L220 5L210 69L8 77L1 1Z" stroke="#6B6C6F" strokeMiterlimit="10" strokeDasharray="9 8" />
+        <text x="50%" y="70%" textAnchor="middle" fontSize="48px" fill="#6B6C6F">
+          {props.children}
+        </text>
       </svg>
+    </Link>
+  )
+}
 
-    </Link >
-  );
-};
-
-export default Button;
+export default Button
